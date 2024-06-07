4e64af28d29c1a085ea68b38ecd16b6a2f62d04a
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-06-07 22:13:10 +0200

    update contentufl/types

--- src/contentful/types/TypeBlogPostMaxSchmitt.ts
+++ src/contentful/types/TypeBlogPostMaxSchmitt.ts
@@ -1,11 +1,23 @@
-import type { ChainModifiers, Entry, EntryFieldTypes, EntrySkeletonType, LocaleCode } from "contentful";
+import type {
+  ChainModifiers,
+  Entry,
+  EntryFieldTypes,
+  EntrySkeletonType,
+  LocaleCode,
+} from "contentful";
 
 export interface TypeBlogPostMaxSchmittFields {
-    title: EntryFieldTypes.Symbol;
-    slug: EntryFieldTypes.Symbol;
-    body: EntryFieldTypes.RichText;
-    image: EntryFieldTypes.AssetLink;
+  title: EntryFieldTypes.Symbol;
+  slug: EntryFieldTypes.Symbol;
+  body: EntryFieldTypes.RichText;
+  image: EntryFieldTypes.AssetLink;
 }
 
-export type TypeBlogPostMaxSchmittSkeleton = EntrySkeletonType<TypeBlogPostMaxSchmittFields, "blogPostMaxSchmitt">;
-export type TypeBlogPostMaxSchmitt<Modifiers extends ChainModifiers, Locales extends LocaleCode> = Entry<TypeBlogPostMaxSchmittSkeleton, Modifiers, Locales>;
+export type TypeBlogPostMaxSchmittSkeleton = EntrySkeletonType<
+  TypeBlogPostMaxSchmittFields,
+  "blogPostMaxSchmitt"
+>;
+export type TypeBlogPostMaxSchmitt<
+  Modifiers extends ChainModifiers,
+  Locales extends LocaleCode = LocaleCode,
+> = Entry<TypeBlogPostMaxSchmittSkeleton, Modifiers, Locales>;
--- src/contentful/types/index.ts
+++ src/contentful/types/index.ts
@@ -1,7 +1,5 @@
-export type { TypeAuthor, TypeAuthorFields, TypeAuthorSkeleton } from "./TypeAuthor";
-export type { TypeBlogPostMaxSchmitt, TypeBlogPostMaxSchmittFields, TypeBlogPostMaxSchmittSkeleton } from "./TypeBlogPostMaxSchmitt";
-export type { TypePortfolioCertificates, TypePortfolioCertificatesFields, TypePortfolioCertificatesSkeleton } from "./TypePortfolioCertificates";
-export type { TypePortfolioDaniel, TypePortfolioDanielFields, TypePortfolioDanielSkeleton } from "./TypePortfolioDaniel";
-export type { TypePost, TypePostFields, TypePostSkeleton } from "./TypePost";
-export type { TypeProjectsContentfulJohnSmilga, TypeProjectsContentfulJohnSmilgaFields, TypeProjectsContentfulJohnSmilgaSkeleton } from "./TypeProjectsContentfulJohnSmilga";
-export type { TypeSimpleRecipesGatsbyJohnSmilga, TypeSimpleRecipesGatsbyJohnSmilgaFields, TypeSimpleRecipesGatsbyJohnSmilgaSkeleton } from "./TypeSimpleRecipesGatsbyJohnSmilga";
+export type {
+  TypeBlogPostMaxSchmitt,
+  TypeBlogPostMaxSchmittFields,
+  TypeBlogPostMaxSchmittSkeleton,
+} from "./TypeBlogPostMaxSchmitt";
